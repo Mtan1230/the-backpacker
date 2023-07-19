db24c7e957c8b3ea497c9f71bf580ec6c83697f8
Saeeda <saeedabatool39@gmail.com>
2023-07-19 11:56:12 -0400

    added trip-routes.js to link it to the trip.handlebars, also installed npm axios package for trip apis, and added a createtrip-routes.js file to hold the api's

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const travellerRoutes = require('./traveller-routes');
+const createtripRoutes = require('./createtrip-routes');
 const postRoutes = require('./post-routes');
 
 router.use('/travellers', travellerRoutes);
+router.use('/trip', createtripRoutes)
 router.use('/post', postRoutes);
 
 module.exports = router;
