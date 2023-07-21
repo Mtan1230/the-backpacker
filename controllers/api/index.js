21d6ee259518ceb18923134ee94d750b84f96063
Mtan1230 <m.tan1230@gmail.com>
2023-07-21 13:03:25 -0400

    fix errer

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const travellerRoutes = require('./traveller-routes');
-const createtripRoutes = require('./createtrip-routes');
 const postRoutes = require('./post-routes');
 const locationRoutes = require('./location-routes');
 const createtripRoutes = require('./createtrip-routes');
