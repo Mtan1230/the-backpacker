f5f6e7d7c957e7241ccc835981cf24b5bc6907f2
Mtan1230 <m.tan1230@gmail.com>
2023-07-22 13:30:00 -0400

    fix error in plan-trip-route

--- controllers/api/createtrip-routes.js
+++ controllers/api/createtrip-routes.js
@@ -1,60 +1,57 @@
 const router = require('express').Router();
 const Amadeus = require('amadeus');
+require('dotenv').config();
 
 
 // CREATE a trip by searching cheapest flights using Amadeus
 router.post('/search', async (req, res) => {
-
-  const amadeus = new Amadeus({
-    clientId: 'f7FtPkofIucCEZGd1j1nQ2d2HlKHS3rG',
-    clientSecret: 'jeAA44fFVLSyGIsX',
-    Url: "https://test.api.amadeus.com/v2"
-  });
-
-  const amadeusleavinglocations = await amadeus.referenceData.locations
-    .get({
-      keyword: req.body.leavingFrom,
-      subType: Amadeus.location.any,
-    })
-  const amadeusdestinationlocations = await amadeus.referenceData.locations
-    .get({
-      keyword: req.body.destination,
-      subType: Amadeus.location.any,
-    })
-  const amadeus_leavinglocation_one = amadeusleavinglocations.data[0]
-  const amadeus_destinationlocation_one = amadeusdestinationlocations.data[0]
-
-  // Confirm availability and price for 1 adult
-  let leavingFrom = amadeus_leavinglocation_one.iataCode;
-  let destination = amadeus_destinationlocation_one.iataCode;
-  let departure = req.body.departure;
-  let number_traveller = 1;
-
-  amadeus.shopping.flightOffersSearch.get({
-    originLocationCode: leavingFrom,
-    destinationLocationCode: destination,
-    departureDate: departure,
-    adults: number_traveller
-  })
-    .then(function (flightOffersSearchResponse) {
-      return amadeus.shopping.flightOffers.pricing.post(
-        JSON.stringify({
-          'data': {
-            'type': 'flight-offers-pricing',
-            'flightOffers': [flightOffersSearchResponse.data[0]]
-          }
-        })
-      )
-    })
-    .then(function (data) {
-      console.log(JSON.stringify(data));
-      res.status(200).json(data);
-    })
-    .catch(error => {
-      console.error(error);
-      res.status(500).json({ error, message: "Amadeus failed to get the flight offers search" });
+  try {
+    const amadeus = new Amadeus({
+      clientId: process.env.AMADEUS_CLIENT_ID,
+      clientSecret: process.env.AMADEUS_CLIENT_SECRET,
+      Url: 'https://test.api.amadeus.com/v2'
     });
-    
+
+    const amadeusleavinglocations = await amadeus.referenceData.locations
+      .get({
+        keyword: req.body.leavingFrom,
+        subType: Amadeus.location.any,
+      });
+    const amadeusdestinationlocations = await amadeus.referenceData.locations
+      .get({
+        keyword: req.body.destination,
+        subType: Amadeus.location.any,
+      });
+    const amadeus_leavinglocation_one = amadeusleavinglocations.data[0];
+    const amadeus_destinationlocation_one = amadeusdestinationlocations.data[0];
+
+    // Confirm availability and price for 1 adult
+    let leavingFrom = amadeus_leavinglocation_one.iataCode;
+    let destination = amadeus_destinationlocation_one.iataCode;
+    let departure = req.body.departure;
+    let number_traveller = 1;
+
+    const flightOffersSearchResponse = await amadeus.shopping.flightOffersSearch.get({
+      originLocationCode: leavingFrom,
+      destinationLocationCode: destination,
+      departureDate: departure,
+      adults: number_traveller
+    });
+
+    const data = await amadeus.shopping.flightOffers.pricing.post(
+      JSON.stringify({
+        'data': {
+          'type': 'flight-offers-pricing',
+          'flightOffers': [flightOffersSearchResponse.data[0]]
+        }
+      })
+    );
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error, message: 'Amadeus failed to get the flight offers search' });
+  }
 });
 
 module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const travellerRoutes = require('./traveller-routes');
 const postRoutes = require('./post-routes');
 const locationRoutes = require('./location-routes');
+const createtripRoutes = require('./createtrip-routes');
 
 router.use('/travellers', travellerRoutes);
-router.use('/trip', createtripRoutes);
 router.use('/post', postRoutes);
 router.use('/locations', locationRoutes);
 router.use('/trip', createtripRoutes);
