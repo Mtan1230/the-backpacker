61af9f440d26c720845221f41f35d6aef6245530
mluron-ArxFjs <ms.luron@nycacademy.org>
2023-07-22 10:23:26 -0400

    fixed api/index.js with createtripRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const travellerRoutes = require('./traveller-routes');
 const postRoutes = require('./post-routes');
 const locationRoutes = require('./location-routes');
+const createtripRoutes = require('./createtrip-routes');
 
 router.use('/travellers', travellerRoutes);
 router.use('/trip', createtripRoutes);
